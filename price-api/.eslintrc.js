de5f75423698f5c69dc659574f5bb3ca5e5366bb
JRaams <47593569+JRaams@users.noreply.github.com>
2021-01-09 11:16:17 +0100

    [PA] Add eslint + prettier config

---
+++ price-api/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['prettier', 'standard'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    semi: 0,
+    'comma-dangle': 0,
+  },
+};
--- price-api/server.js
+++ price-api/server.js
@@ -1,19 +1,21 @@
-const polka = require('polka')
-const { json } = require('body-parser')
-const { graphqlHTTP } = require('express-graphql')
+const polka = require('polka');
+const { json } = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
 const { schema, rootValue } = require('./graphql');
 
-const PORT = process.env.PORT || 3000;
-const PRODUCTION = !process.env.PRODUCTION || false;
+const { PORT = 3000, PRODUCTION = false } = process.env;
 
 polka()
   .use(json())
-  .use('/graphql', graphqlHTTP({
-    schema,
-    rootValue,
-    graphiql: PRODUCTION
-  }))
-  .listen(PORT, err => {
+  .use(
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      rootValue,
+      graphiql: !PRODUCTION,
+    })
+  )
+  .listen(PORT, (err) => {
     if (err) throw err;
-    console.log('API Server listening on :', PORT)
-  })
+    console.log('API Server listening on :', PORT);
+  });
