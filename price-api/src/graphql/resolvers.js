5a07c8569c3248adb6da76eacd2d27aaba90dc80
JRaams <47593569+JRaams@users.noreply.github.com>
2021-01-09 12:15:55 +0100

    [PA] Add assetgroup resolvers

--- price-api/src/graphql/resolvers.js
+++ price-api/src/graphql/resolvers.js
@@ -1,5 +1,7 @@
 const { taskResolver } = require('../entities/tasks');
+const { assetGroupResolver } = require('../entities/assetgroups');
 
 module.exports = {
   ...taskResolver,
+  ...assetGroupResolver,
 };
--- price-api/src/graphql/schema.js
+++ price-api/src/graphql/schema.js
@@ -6,9 +6,16 @@ const schema = buildSchema(`
     name: String!
     complete: Boolean!
   }
+  type AssetGroup {
+    id: Int!
+    name: String!
+    stars: Int! 
+  }
   type Query {
     tasks: [Task]
     task(id: Int!): Task
+    assetGroups: [AssetGroup]
+    assetGroup(id: Int!): AssetGroup
   }
 `);
 
